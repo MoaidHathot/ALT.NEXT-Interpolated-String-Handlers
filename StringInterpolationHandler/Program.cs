209a563c7b9d41625853485de734dd4b8e179549
Moaid Hathot <moaidhathot@microsoft.com>
2023-05-01 17:25:41 +0300

    Resetting Program.cs

--- StringInterpolationHandler/Program.cs
+++ StringInterpolationHandler/Program.cs
@@ -1,11 +1,3 @@
-﻿// See https://aka.ms/new-console-template for more information
-using Microsoft.Extensions.Logging;
+﻿using Dumpify;
 
-var logger = new Logger(LogLevel.Information);
-
-var firstName = "Moaid";
-var lastName = "Hathot";
-
-logger.Log(LogLevel.Information, $"{firstName} {lastName}");
-
-
+"Hello, Alt.NET".Dump();
